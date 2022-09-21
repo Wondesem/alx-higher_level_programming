da573ef83801e7613a557fd87ed3568f34ab3ffb
wondosen <wseminew50@gmail.com>
2022-09-20 23:03:18 -0700

    second chance attempt

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+mport random
+number = random.randint(-10000, 10000)
+if number >= 0:
+    las_digit = number %  10
+else:
+    las_digit = number % (-10)
+print("Last digit of", number, "is", las_digit, end=" ")
+if las_digit > 5:
+    print("and is greater than 5")
+elif las_digit == 0:
+    print("and is 0")
+else:
+    print("and is less than 6 and not 0")
