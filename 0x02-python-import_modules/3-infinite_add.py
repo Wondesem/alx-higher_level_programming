0ca8fe3e240b94489b2553ba685c59351055e2b4
wondosen <wseminew50@gmail.com>
2022-09-22 08:03:45 -0700

    initial commit ontask 0x02

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    sum = 0
-    if len(sys.argv) is 1:
-        print("{}".format(sum))
-    elif len(sys.argv) > 1:
-        for x in range(1, len(sys.argv)):
-            sum = sum + int(sys.argv[x])
-            print("{}".format(sum))
+    from sys import argv
+    add = 0
+    for s in argv[1:]:
+        add += int(s)
+        print("{:d}".format(add))
