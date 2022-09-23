cb2b11a8289e215bb9ab1d2aab5750cbc753bc1c
wondosen <wseminew50@gmail.com>
2022-09-22 18:14:37 -0700

    initial commit on task 0x02

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import sys
     sum = 0
-    if len(sys.argv) is 1:
+    if len(sys.argv) == 1:
         print("{}".format(sum))
     elif len(sys.argv) > 1:
         for x in range(1, len(sys.argv)):
