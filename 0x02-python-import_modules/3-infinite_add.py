3a8381ed2ae1ad5d4d86e323ebc19ec6b3c703ca
wondosen <wseminew50@gmail.com>
2022-09-24 09:22:44 -0700

    second attempt on task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,6 +2,9 @@
 if __name__ == "__main__":
     import sys
     sum = 0
-    for x in range argv[1:]:
-        sum = sum + int(x)
-        print("{:d}".format(sum))
+    if len(sys.argv) is 1:
+        print("{}".format(sum))
+    elif len(sys.argv) > 1:
+        for x in range(1, len(sys.argv)):
+            sum = sum + int(sys.argv[x])
+            print("{}".format(sum))
