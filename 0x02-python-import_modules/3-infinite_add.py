7463dc3d9bb8f7e38cc410fdb63c9af7f50a5063
wondosen <wseminew50@gmail.com>
2022-09-25 11:33:34 -0700

    second commit on task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     sum1 = 0
     if len(sys.argv) == 1:
         print("{}".form(sum1))
-    else:
+    elif len(sys.argv) > 1:
     for i in range(1, len(sys.argv)):
         sum1 += int(sys.argv[i])
     print("{}".format(sum1))
