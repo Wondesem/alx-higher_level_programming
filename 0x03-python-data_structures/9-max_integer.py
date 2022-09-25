e6946c402ab1471e1c2ba749bb69874173f781a6
wondosen <wseminew50@gmail.com>
2022-09-25 10:11:04 -0700

    ininitail commit on task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if len(my_list) == 0:
-        return None
+        return (None)
     maxelm = my_list[0]
     for i in range(len(my_list)):
         if my_list[i] > maxelm
         maxelm = my_list[i]
-    return maxelm
-
+    return (maxelm)
