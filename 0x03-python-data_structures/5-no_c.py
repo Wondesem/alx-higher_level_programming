2e26ec6f31e90c27f8480bfbb42d8fc9133d28d4
wondosen <wseminew50@gmail.com>
2022-09-25 08:33:24 -0700

    second commit on task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    char_to_remove = ["c", "C"]
     newStr = ""
     for character in my_string:
-        if character != char_to_remove:
+        if character != "c" and "C":
             newStr += character
     return newStr
