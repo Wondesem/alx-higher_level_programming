41906449210354fe0f962a214f97b90cec7fc60a
wondosen <wseminew50@gmail.com>
2022-09-24 23:10:13 -0700

    This project helps grasp data structure in python

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    nmy_string = ""
+    new_string = ""
     for c in my_string:
-        if c != "c" and c != "c":
-            nmy_string += c
-            return nmy_string
+        if c != 'c' and c != 'C':
+            new_string += c
+            return new_string
