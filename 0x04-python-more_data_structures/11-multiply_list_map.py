052863ea6a43c5015b64cb4f4f1c7407ca6d5136
wondesem <wseminew50@gmail.com>
2022-09-30 11:37:36 -0700

    second try

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-def mutiply_list_map(my_list=[], number=0):
-    my_new_list = map(lambda x: x * number, my_list)
-    return (my_new_list)
+def multiply_list_map(my_list=[], number=0):
+    return (list(map(lambda item: item * number, my_list)))
