b7b4436705b1b0a1d0c28af3d5c39c4a12dbc7e2
wondesem <wseminew50@gmail.com>
2022-09-30 13:08:55 -0700

    second try

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,11 +6,17 @@ def roman_to_int(roman_string):
     roman = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     for i in roman_string:
         n += roman.get(i, 0)
-        subs = {'CM': -200, 'CD': -200, 'XC': -20, 'XL': -20, 'IX': -2, 'IV': -2}
+        subs = {
+                'CM': -200,
+                'CD': -200,
+                'XC': -20,
+                'XL': -20,
+                'IX': -2,
+                'IV': -2
+                }
         chars = ""
     for i in range(0, len(roman_string) - 1):
         chars = roman_string[i] + roman_string[i + 1]
         if chars in subs:
             n += subs[chars]
     return n
-
