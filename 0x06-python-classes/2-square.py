889297a0091dd67ac45a3b92017edaab912a1654
wondesem <wseminew50gmail.com>
2022-10-11 04:17:25 -0700

    commit on task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Define a class named square"""
+
+
+class Square:
+    """Here is a place to class attributes"""
+
+
+def __init__(self, size=0):
+    """Initialize the data with error
+    handling"""
+    if type(size) is not int:
+        raise TypeError('size must be an integer')
+    if size < 0:
+        raise ValueError('size must be >= 0')
+    self.__size = size
