b657ade20bfaceb7af594da64c801fe9a126c62c
wondesem <wseminew50gmail.com>
2022-10-11 04:28:25 -0700

    commit on task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,12 +5,10 @@
 class Square:
     """Here is a place to class attributes"""
 
-
-def __init__(self, size=0):
-    """Initialize the data with error
-    handling"""
-    if type(size) is not int:
-        raise TypeError('size must be an integer')
-    if size < 0:
-        raise ValueError('size must be >= 0')
-    self.__size = size
+    def __init__(self, size=0):
+        """Initialize the data with errorhandling"""
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
