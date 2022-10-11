4cef700edfde848a81022fb4492bb98fc28406e0
wondesem <wseminew50gmail.com>
2022-10-11 03:59:26 -0700

    commit on task 2

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Definne a class of square"""
+
+
+class Square:
+    """This place is reserved for class atributes"""
+
+    """Define instance attributes"""
+    def __init__(self, size):
+        """ Here is the place of instance attributes such
+        as data, arguments and return"""
+        self._size = size
