c5095adbf4bebc201325d388a22d018ea2476e34
wondesem <wseminew50gmail.com>
2022-10-12 07:52:54 -0700

    second chance commit on task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
+"""defines a class of square"""
 
 
 class Square:
-    """The class that defines a square"""
+    """Represents a square
+        Attributes:
+        __size (int): size of a side of the square
+          """
     def __init__(self, size=0):
-        """Constructor of the Square class"""
+        """initializes the square
+            Args:
+                size (int): size of a side of the square
+                    Returns:
+                    None
+                     """
         self.size = size
 
     def size(self):
