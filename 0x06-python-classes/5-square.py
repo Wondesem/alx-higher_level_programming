75b82920ba4d148b8b70db3d96d0195146ce376b
wondesem <wseminew50gmail.com>
2022-10-12 11:42:51 -0700

    second attempt on task 5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -3,7 +3,7 @@
 
 
 class Square:
-    """The class that defines a square"""
+    """Represent a square."""
 
     def __init__(self, size=0):
         """Constructor of the Square class"""
@@ -19,12 +19,14 @@ class Square:
         """The __size setter"""
         if value:
             if isinstance(value, int) is not True:
-                raise TypeError("size must be an integer")
+                aise TypeError("size must be an integer")
+                return
+
+            if value < 0:
+                raise ValueError("size must be >= 0")
             return
-        if value < 0:
-            raise ValueError("size must be >= 0")
-        return
-    self.__size = value
+
+        self.__size = value
 
     def area(self):
         """Returns the size of the square"""
