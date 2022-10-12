ec605052783830717acd0a448adbdec3fd0769ed
wondesem <wseminew50gmail.com>
2022-10-12 08:22:10 -0700

    second chance commit on task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,32 +1,43 @@
 #!/usr/bin/python3
-"""defines a class of square"""
+"""Defines a class Square"""
 
 
 class Square:
     """Represents a square
         Attributes:
-        __size (int): size of a side of the square
-          """
+            __size (int): size of a side of the square
+                        """
     def __init__(self, size=0):
         """initializes the square
             Args:
                 size (int): size of a side of the square
-                    Returns:
-                    None
-                     """
+            Returns:
+              None
+              """
         self.size = size
 
-    def size(self):
-        """The __size getter"""
-        return (self.__size)**2
+    def area(self):
+        """calculates the square's area
+            Returns:
+                The area of the square
+                """
+        return (self.__size) ** 2
 
     @property
     def size(self):
-        """The __size setter"""
-        return size.__size
+        """getter of __size
+            Returns:
+                The size of the square
+                  """
+        return self.__size
 
     @size.setter
     def size(self, value):
+        """setter of __size
+            Args:
+                value (int): the size of a size of the square
+             Returns:
+                 None"""
         if type(value) is not int:
             raise TypeError("size must be an integer")
         else:
