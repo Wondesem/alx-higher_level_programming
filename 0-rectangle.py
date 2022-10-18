9e3669772d6a41db38323b317f4adb1cbfc109ef
wondesem <wseminew50gmail.com>
2022-10-17 22:46:00 -0700

    first attempt

---
+++ 0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# 0-rectangle.py
+""" File name : 0-rectangle.py
+This rectange defines an empty rectangle class.
+
+Importing module is not allowed.
+"""
+
+
+class Rectangle(object):
+    """Rectangle: Define new class"""
+    pass
