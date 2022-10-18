10656da0f64948ac26e0c5b1444938083224da6a
wondesem <wseminew50gmail.com>
2022-10-18 07:01:18 -0700

    advanced task first attempt

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# 101-locked_class.py
+""" File name : 101-locked_class.py
+Low memory cost: Defines a locked class
+Importing any module is not allowed
+        """
+
+class LockedClass:
+    """Prevent the user from instantiating new LockedClass attributes.
+    prevents the user from dynamically creating new instance attributes
+    except if the new instance attribute is called first_name
+    """
+    __lots__ = ["first_name"]
