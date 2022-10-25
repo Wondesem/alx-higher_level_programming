bdb9b94e96c82cae060b4f8898fa00c815f885af
wondesem <wseminew50gmail.com>
2022-10-25 10:50:56 -0700

    task 5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-# 5-to_json_string.py
-""" File name : 5-to_json_string.py
-"""
+# 7-save_to_json_file.py
+"""Defines a JSON file-writing function."""
 import json
 
 
-def to_json_string(my_obj):
-    """to_json_string  returns the JSON representation of an object (string)
-    Args:
-        my_obj (obj): any object for example list, dict
-    """
-    return(json.dumps(my_obj))
+def save_to_json_file(my_obj, filename):
+    """Write an object to a text file using JSON representation."""
+    with open(filename, "w") as f:
+        json.dump(my_obj, f)
