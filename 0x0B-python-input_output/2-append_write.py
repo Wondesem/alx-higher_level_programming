57b89d65c19a695fce66136cf8bc8f4cc62a44ff
wondesem <wseminew50gmail.com>
2022-10-25 10:37:23 -0700

    task 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-"""
-Contains the "read_lines" function
+# 4-append_write.py
+""" File name : 4-append_write.py
 """
 
 
-def read_lines(filename="", nb_lines=0):
-    """reads n lines of a text file (UTF8) and prints it to stdout"""
-    with open(filename, 'r', encoding='utf-8') as f:
-        if nb_lines <= 0:
-            print(f.read(), end='')
-            return
-        i = 0
-        for i, line in enumerate(f):
-            if i == nb_lines:
-                break
-            print(line, end='')
+def append_write(filename="", text=""):
+    """append_write appends a string at the end of a text file (UTF8)
+    Args:
+        filename (str): Defaults to "".
+        text (str): text to add. Defaults to "".
+    """
+    with open(filename, 'a', encoding='utf-8') as f:
+        return f.write(text)
