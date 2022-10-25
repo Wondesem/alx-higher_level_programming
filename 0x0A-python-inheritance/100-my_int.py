7adf9fe0e8b9d167b4f998c0c4a75be45c6a3c39
wondesem <wseminew50gmail.com>
2022-10-25 07:22:35 -0700

    task 100

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+a class MyInt that inherits from int
+"""
+
+
+class MyInt(int):
+    """
+    Class MyInt that inherits from int"""
+    def __eq__(self, x):
+        """eq"""
+        return not super().__eq__(x)
+
+    def __ne__(self, x):
+        """not eq"""
+        return not super().__ne__(x)
