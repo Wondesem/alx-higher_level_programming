2b2262b2ce56dc41952ed29c835a5c626e4156fd
wondesem <wseminew50gmail.com>
2022-10-26 05:56:27 -0700

    task 7 correction

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,9 +8,10 @@ if __name__ == "__main__":
     load_from_json_file = \
         __import__('6-load_from_json_file').load_from_json_file
 
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+open("add_item.json", "a")
+
+try:
+    lst = load_from_json_file("add_item.json")
+except ValueError:
+    lst = []
+add_item.json(lst + sys.argv[1:], "add_item.json")
