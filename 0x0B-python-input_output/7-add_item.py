6775f06b307b62ff0c753444fac66b0b5335a891
wondesem <wseminew50gmail.com>
2022-10-26 06:03:17 -0700

    task 7 correction

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -14,4 +14,4 @@ try:
     lst = load_from_json_file("add_item.json")
 except ValueError:
     lst = []
-save_to_json_file(l + sys.argv[1:], "add_item.json")
+save_to_json_file(lst + sys.argv[1:], "add_item.json")
