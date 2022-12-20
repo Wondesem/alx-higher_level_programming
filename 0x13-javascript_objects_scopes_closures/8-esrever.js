4b7422b2dd15847d011b5d8200bfcc27503ac981
wondesem <wseminew50gmail.com>
2022-12-20 10:29:10 -0800

    task 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,4 +1,9 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  return list.sort(() => 1);
+  let i = 0;
+  let j = list.length - 1;
+  for (i = 0; i < list.length / 2; i++, j--) {
+    [list[i], list[j]] = [list[j], list[i]];
+  }
+  return list;
 };
