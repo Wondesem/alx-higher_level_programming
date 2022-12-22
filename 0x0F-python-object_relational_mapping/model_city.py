1654fe0dfde1b7fc0b47c1323a9e8118fd38d25f
wondesem <wseminew50gmail.com>
2022-12-22 08:27:16 -0800

    Cities in state

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -4,23 +4,17 @@ deletes all State objects with a name containing
 the letter a from the database hbtn_0e_6_usa"""
 import sys
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from model_state import State
 from model_city import City
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(
-                                   sys.argv[1],
-                                   sys.argv[2],
-                                   sys.argv[3]
-                                   ),
-                           pool_pre_ping=True
-                           )
-    session = Session(engine)
-    re = session.query(City, State).filter(City.state_id == State.id)\
-            .order_by(City.id).all()
-    for C, x in re:
-        print("{}: ({}) {}".format(x.name, C.id, C.name))
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for row in session.query(State, City).join(City).order_by(City.id):
+        print("{:}: ({:}) {:}".format(row[0].name, row[1].id, row[1].name))
     session.commit()
     session.close()
