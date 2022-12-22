c1072b09d53c1c15e6fdac6605f1a9cb542efe81
wondesem <wseminew50gmail.com>
2022-12-22 07:32:13 -0800

    12-model_state_update_id_2.py

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,31 +1,25 @@
 #!/usr/bin/python3
 """
-changes the name of a State
-object from the database hbtn_0e_6_usa
-"""
-
+changes the name of a State object
+from the database hbtn_0e_6_usa"""
 import sys
-from model_state import Base, State
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import update
+from sqlalchemy.orm import Session
+from model_state import Base, State
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-
-    # Initialize engine
-    Basse.metadata.create_all(engine)
-
-    # Initialize session
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    # Update record
-    update = session.query(State).filter(State.id == 2).one()
-    update.name = "New Mexico"
+    engine = create_engine(
+                           'mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(
+                                   sys.argv[1],
+                                   sys.argv[2],
+                                   sys.argv[3]
+                                   ),
+                           pool_pre_ping=True
+                           )
+    session = Session(engine)
+    re = session.query(State).filter_by(id=2).first()
+    re.name = "New Mexico"
+    re = session.add(re)
     session.commit()
-
-    # Close session
     session.close()
