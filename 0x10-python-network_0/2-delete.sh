80f9a1af5173bac3699f0286137608edefd3353b
wondesem <wseminew50gmail.com>
2023-01-16 03:10:34 -0800

    task 2

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 i#!/bin/bash
-# sends a DELETE request to the URL and displays the body of the response
-curl -sX "DELETE" $1
+# sends a DELETE request and displays the body of the response
+curl -sX DELETE "$1"
