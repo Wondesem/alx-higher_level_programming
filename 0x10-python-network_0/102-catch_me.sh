e2ebcd613f9fc455e5ed26544d3acab25af11c91
wondesem <wseminew50gmail.com>
2023-01-16 04:29:29 -0800

    task 102

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # makes a request that causes serves to send "You got me!" as a response
-curl -sLX PUT "You got me!" -d "user_id=98" 0.0.0.0:5000/catch_me
+curl -sL -X PUT -H "Origin: ALXSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
