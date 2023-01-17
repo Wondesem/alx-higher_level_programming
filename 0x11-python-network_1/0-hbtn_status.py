7b0d3a7033cd71b2d15b04acd7dd8bcac8147dfb
wondesem <wseminew50gmail.com>
2023-01-17 05:04:09 -0800

    task 4

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-''' fetches https://intranet.hbtn.io/status '''
+'''
+Script that fetches https://intranet.hbtn.io/status
+'''
+from urllib import request
 
-if __name__ == '__main__':
-    import urllib.request
-    req = urllib.request.Request('https://intranet.hbtn.io/status')
-    with urllib.request.urlopen(req) as response:
-        html = response.read()
-
-    print("Body response:")
-    print("\t- type: {}".format(html.__class__))
-    print("\t- content: {}".format(html))
-    print("\t- utf8 content: {}".format(html.decode('ascii')))
+url = 'https://intranet.hbtn.io/status'
+with request.urlopen(url) as response:
+    the_page = response.read()
+    print('Body response:')
+    print('\t- type: {}'.format(type(the_page)))
+    print('\t- content: {}'.format(the_page))
+    print('\t- utf8 content: {}'.format(the_page.decode('utf-8')))
