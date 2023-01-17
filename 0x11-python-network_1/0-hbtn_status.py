871b16404f381c96aeaec30607c39d18b31cab38
wondesem <wseminew50gmail.com>
2023-01-17 02:45:43 -0800

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,7 +2,8 @@
 """What's my status?"""
 import urllib.request
 
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as url:
-    s = url.read()
-    req = "Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
-    print(req.format(type(s), s, s.decode('utf-8')))
+with uri.urlopen('https://intranet.hbtn.io/status') as res:
+    res = res.read()
+
+print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(res), res))
+print('\t- utf8 content: {}'.format(str(res, 'utf-8')))
