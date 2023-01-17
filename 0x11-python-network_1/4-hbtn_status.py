442199019830acb8995b9d57ffc71e174ec63836
wondesem <wseminew50gmail.com>
2023-01-17 04:50:16 -0800

    task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""
-same as 0-hbtn_status with requests model
-"""
+''' fetches https://intranet.hbtn.io/status '''
+import requests
 
-if __name__ == '__main__':
-    import requests
-    html = requests.get('https://intranet.hbtn.io/status')
+
+if __name__ == "__main__":
+    req = requests.get('https://intranet.hbtn.io/status')
+    text = req.text
     print('Body response:')
-    print('\t- type: {}'.format(type(url.text)))
-    print('\t- content: {}'.format(url.text))
+    print('\t- type: {}'.format(type(text)))
+    print('\t- content: {}'.format(text))
